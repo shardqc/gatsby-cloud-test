5452b4d423eb6cb31e0ed57a8ff056c44d1fb1a5
Xavier Bourgault <xavierbourgault92@gmail.com>
2022-09-03 15:35:08 -0400

    creation method get3cat

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,21 @@ import { PageHeader,Carousel,Heading,Image,Anchor } from "grommet"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
+async function get3Cats() {
+    const catsArray = [];
+    for (let index = 0; index < 3; index++) {
+        const response = fetch("https://cataas.com/cat");
+        const data = await (await response).json;
+        catsArray[index] = data;
+        
+    }
+    return catsArray;    
+}
+console.log(catsArray);
 const IndexPage = () =>{
     return(
     <main>
-
+        
         <Heading >Carousel with 3 random cat photos from the Cataas API</Heading>
 
     </main>
