3487c37ff17eee7512c14a7719577951d806c5a1
Xavier Bourgault <xavierbourgault92@gmail.com>
2022-09-03 15:21:10 -0400

    wip..

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,18 @@ import { PageHeader,Carousel,Heading,Image } from "grommet"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
-IndexPage = () =>{
+const IndexPage = () =>{
     return(
-    <Main>
+    <main>
         <PageHeader
             title="Photo Website"
             subtitle="Built using Gatsby cloud!"
             parent={<Anchor />}
         />
-        <Heading responsive={true}>Carousel with 3 random cat photos from the Cataas API</Heading>
+        <Heading >Carousel with 3 random cat photos from the Cataas API</Heading>
 
-    </Main>
-    );
+    </main>
+    )
   
 }
 
